faef60fc87afdc938fc58792d8ac6a2fd3bcf8bb
Takumi Sato <takumi2003711@gmail.com>
2026-06-19 09:46:52 +0900

    リダイレクト処理修正2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 
 // 自分が「旧URL」で開かれているかチェックし、旧URLなら新URLへ強制ワープ！
-const OLD_DOMAIN = "progress-dashboard-backend.onrender.com";
+const OLD_DOMAIN = "progress-dashboard-frontend.onrender.com";
 const NEW_DOMAIN = "progress-dashboard-refact-2.onrender.com";
 
 if (window.location.hostname === OLD_DOMAIN) {
